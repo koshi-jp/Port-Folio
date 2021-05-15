9a7aa722d04acf45284b4bfc8932f53ab37e6ca1
koshi-jp <koushi0324@gmail.com>
2021-05-15 00:29:38 +0000

    モデルとコントローラー作成

---
+++ db/migrate/20210515002401_create_posts.rb
@@ -0,0 +1,12 @@
+class CreatePosts < ActiveRecord::Migration[5.2]
+  def change
+    create_table :posts do |t|
+      t.integer :member_id
+      t.string :title
+      t.text :body
+      t.text :image_id
+
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_10_214650) do
+ActiveRecord::Schema.define(version: 2021_05_15_002401) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -52,6 +52,15 @@ ActiveRecord::Schema.define(version: 2021_05_10_214650) do
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_members_on_email", unique: true
     t.index ["reset_password_token"], name: "index_members_on_reset_password_token", unique: true
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "member_id"
+    t.string "title"
+    t.text "body"
+    t.text "image_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
