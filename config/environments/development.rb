ee8567af76f9ff023b02c3ab2ba1ba3c9b57c38c
koshi-jp <koushi0324@gmail.com>
2021-05-22 07:13:09 +0000

    お問い合わせ機能完成

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,13 +60,14 @@ Rails.application.configure do
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
 
   # お問い合わせ機能
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.gmail.com',
   port:                  587,
   domain:               'gmail.com',
-  user_name:             ENV[GMAIL_ADRESS],
-  password:              ENV[GMAIL_PASSWARD],
+  user_name:             ENV['GMAIL_ADRESS'],
+  password:              ENV['GMAIL_PASSWARD'],
   authentication:       'plain',
   enable_starttls_auto:  true
   }
