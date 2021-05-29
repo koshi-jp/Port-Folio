c6bd7028526411aeb49e253fbb5ffeb85a96488b
koshi-jp <koushi0324@gmail.com>
2021-05-29 05:44:10 +0000

    ゲストログイン情報修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -7,6 +7,7 @@ class Public::HomesController < ApplicationController
       member.password = SecureRandom.urlsafe_base64
       member.nickname = "ゲスト"
       member.introduction = "ゲストユーザーです。"
+      member.playing_now = "テトリス"
       # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
       # 例えば name を入力必須としているならば， user.name = "ゲスト" なども必要
     end
