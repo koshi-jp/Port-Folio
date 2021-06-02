8fa1435d9b56a0e6f1da3ca741085eb68c7bdd2a
koshi-jp <koushi0324@gmail.com>
2021-06-02 12:11:43 +0000

    adminpostsのdestroy編集

--- app/controllers/admin/posts_controller.rb
+++ app/controllers/admin/posts_controller.rb
@@ -11,4 +11,10 @@ class Admin::PostsController < ApplicationController
     @posts = Post.all
   end
 
+  def  destroy
+    @post = Post.find(params[:id])
+    @post.destroy
+    redirect_to admin_posts_path,notice: '投稿を削除しました'
+  end
+
 end
